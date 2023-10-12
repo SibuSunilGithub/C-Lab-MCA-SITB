7d8a54a9ff3e7a78a699656b428710d68e41e96e
Sunil Kumar Sahoo <101491022+SibuSunilGithub@users.noreply.github.com>
2023-10-12 22:27:06 +0530

    Paragraph Writing
    Use a single printf statement to print the following paragraph:

---
+++ q2_print_paragraph.c
@@ -0,0 +1,13 @@
+// Use a single printf statement to print the folling paragraph:
+/*
+Good Day!!!
+He shared his "wisdom" with me
+Hurray! We won
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Good Day!!!\nHe shared his \"wisdom\" with me\nHurray! We won");
+    return 0;
+}
